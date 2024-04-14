c656f08aeb3a3e6ee3896b50e56de94eaaa38425
Jahnavi M R <jahnavimr100@gmail.com>
2024-04-15 00:24:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,57 +27,37 @@ const songs = [
     artist: "Atanas Kolev",
   },
   {
-    name: "song-02",
-    displayName: "remarks",
-    artist: "Dim4ou",
-  },
-  {
-    name: "song-03",
-    displayName: "I don't forget you",
-    artist: "Preslava",
-  },
-  {
-    name: "song-04",
-    displayName: "Single",
-    artist: "Galin",
-  },
-  {
-    name: "song-05",
-    displayName: "I've Been Thinking",
-    artist: "Sevenhills",
-  },
-  {
-    name: "song-06",
+    name: "song-2",
     displayName: "The adventures of Mr.Hardy",
     artist: "Yuvan shankar raja",
   },
   {
-    name: "song-07",
+    name: "song-3",
     displayName: "Thuli Thuli",
     artist: "Yuvan shankar raja",
   },
   {
-    name: "song-08",
+    name: "song-4",
     displayName: "Faded",
     artist: "Alan walker",
   },
   {
-    name: "song-09",
+    name: "song-5",
     displayName: "Heeriye ",
     artist: "Arijit Singh",
   },
   {
-    name: "song-10",
+    name: "song-6",
     displayName: "nazm nazm",
     artist: "Arko pravo Mukherjee",
   },
   {
-    name: "song-11",
+    name: "song-7",
     displayName: ":abcdefu",
     artist: "Gayle",
   },
   {
-    name: "song-12",
+    name: "song-8",
     displayName: "bombe heluthaithe",
     artist: "Vijay prakash",
   }
